8b7dc505e84048017218f8dfeea572dcadbd5ee2
giannoul <giannoul88@gmail.com>
2023-06-26 23:53:38 +0300

    Adding env vars

--- app/server.py
+++ app/server.py
@@ -1,14 +1,14 @@
 from flask import Flask, request
-from google.oauth2 import service_account
 from google.cloud import storage
 import os
 from werkzeug.utils import secure_filename
 
+gcs_bucket = os.environ.get('GCS_BUCKET', "")
+storage_client = storage.Client.from_service_account_json('/app/key.json')
 app = Flask(__name__)
 
 def upload_blob_from_stream(bucket_name, file_obj, destination_blob_name):
     """Uploads bytes from a stream or other file-like object to a blob."""
-    storage_client = storage.Client.from_service_account_json('/app/key.json')
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
     file_obj.seek(0)
@@ -21,14 +21,11 @@ def upload_blob_from_stream(bucket_name, file_obj, destination_blob_name):
 def health():
     return { "status": "ok" }
 
-
 @app.route("/api/upload-random", methods=["POST"])
 def upload_random():
     file = request.files['file']
-    print(request.files)
-    upload_blob_from_stream("thisisacompletelyrandombucket324232s", file, secure_filename(file.filename))
+    upload_blob_from_stream(gcs_bucket, file, secure_filename(file.filename))
     return {"status" : secure_filename(file.filename)}
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
